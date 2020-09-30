719a6f6845da46f7334a0e1a1c6a074bc6c8f563
crashn <no_mail@no.no>
2020-09-30 19:02:02 +0300

    add spring boot to the project

---
+++ src/test/java/com/crashn/test/grpcito/GrpcitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crashn.test.grpcito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
